2f81a119a8d16baef5dfb43d5e661bdeb8cd49b5
rmeszes <rmeszes1@gmail.com>
2023-04-19 08:23:00 +0200

    rename

---
+++ vonatjegy_test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <Windows.h>
+
+#include "gtest_lite.h"
+#include "memtrace.h"
+
+#include "Vonat.h"
+#include "List.hpp"
+int main() {
+	//A windowsban a megfelelõ megjelenítéshez
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+	SetConsoleCP(1250);
+	SetConsoleOutputCP(1250);
+#endif
+
+	std::cout << "\nA kód lefutott, Enter a bezáráshoz";
+	std::cin.get();
+	return 0;
+}
