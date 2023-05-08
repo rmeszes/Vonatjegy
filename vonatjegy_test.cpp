d2c3686e1c72b62cdb202d5b8bfe9af8ad941921
Meszes Richárd <79580855+rmeszes@users.noreply.github.com>
2023-05-08 17:23:27 +0200

    skeleton

--- vonatjegy_test.cpp
+++ vonatjegy_test.cpp
@@ -15,35 +15,7 @@ int main() {
 	SetConsoleOutputCP(1250);
 #endif
 
-	TEST(lista, ctor) {
-		ASSERT_NO_THROW(List<size_t> lista);
-	} ENDM
-
-	TEST(list_add, hozzaadas) {
-		List<size_t> lista;
-		lista.Add(1);
-		EXPECT_EQ(1, *lista[0]);
-	} ENDM
-
-	TEST(vonat, ctor) {
-		ASSERT_NO_THROW(Vonat v(1000, 2, 3));
-	} ENDM
-
-	TEST(vonat_ertekek, getters) {
-		Vonat v(1001, 4, 100);
-		EXPECT_EQ(1001, v.getVonatSzam());
-	} ENDM
-
-	TEST(hely, foglalas) {
-		Vonat v(1002, 3, 2);
-		unsigned int hely[2];
-		v.findSeat(hely);
-		EXPECT_EQ(100, hely[0]);
-		EXPECT_EQ(1, hely[1]);
-	} ENDM
-
-	List<Vonat> lista;
-	lista.Add(Vonat(1000, 10, 60));
+	//tesztek ide
 
 
 	std::cout << "\nA kód lefutott, Enter a bezáráshoz";
